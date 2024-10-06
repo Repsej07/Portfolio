47a2b423327dc5eddc14d8819935b410933f51e7
Repsej07 <Jespermeuzelaar@gmail.com>
2024-10-06 23:34:59 +0200

    Added file structure

--- index.php
+++ index.php
@@ -2,10 +2,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
 </head>
 <body>
-    <h1>ello world</h1>
-    
 </body>
 </html>
---
+++ views/Layout/Header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    
+    </head>
+    <body>
+    <div class="main-footer">
+
+
+    </div>
+    </body>
+</html>
+
