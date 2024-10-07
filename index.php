df66e692ffdb618c0ee44910cc3d969778fcb2db
Repsej07 <Jespermeuzelaar@gmail.com>
2024-10-07 10:39:15 +0200

    Almost finished the header content. added the logo and the menu svg

--- index.php
+++ index.php
@@ -1,9 +1,2 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-<body>
-</body>
-</html>
+<?php 
+require "./views/Layout/Header.php";
--- views/Layout/Header.php
+++ views/Layout/Header.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-    
-    </head>
-    <body>
-    <div class="main-footer">
 
+<head>
+    <link rel="stylesheet" href="views/Css/styles.css">
+</head>
 
+<body>
+    <div class="main-header">
+        <div class="logo-container">
+            <span class="fName-logo">Jesper</span>
+            <span class="lName-logo">Meuzelaar</span>
+        </div>
+        <div class="menu">
+        <svg width="30" height="30" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10 40H70M10 20H70M10 60H70" stroke="black" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+        </div>
     </div>
-    </body>
-</html>
+</body>
 
+</html>
