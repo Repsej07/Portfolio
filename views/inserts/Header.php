96ce7ba4c6776e1ca854985b1f2f75047076ec71
Repsej07 <Jespermeuzelaar@gmail.com>
2024-10-07 23:49:00 +0200

    last commit of the day, added the navbar, and busy with the button

--- views/inserts/Header.php
+++ views/inserts/Header.php
@@ -3,16 +3,38 @@
 
 <head>
     <link rel="stylesheet" href="views/Css/styles.css">
-    <script src="views/css/styles.css"></script>
-    
+    <script src="views/JavaScript/links.js"></script>
+    <script src="views/JavaScript/buttons.js"></script>
+
 </head>
 
 <body>
- <div class="nav-bar">
-            <a class="nav-item-1"href="views/main.view.php">Portfolio</a>
-            <a class="nav-item-2"href="views/about.view.php">About</a>
-            <a class="nav-item-3"href="views/contact.view.php">Contact</a>
-            <div class="icons-header">
+
+    <div class="main-header">
+        <div class="backdrop-header"></div>
+        <div class="logo-container" onclick="homelink()">
+            <span class="fName-logo">Jesper</span>
+            <span class="lName-logo">Meuzelaar</span>
+        </div>
+        <div onclick="opennav()"class="menu">
+            <svg  width="30" height="30" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path id="menu" d="M10 40H70M10 20H70M10 60H70"  />
+            </svg>
+        </div>
+        
+    </div>
+    <div class="overlay" id="overlay" >
+    <!-- <div id="svg-container" onclick="closenav()"class="svg-container"><svg class="close-button" width="60" height="60" viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg">
+            <path d="M21.3337 63.3334L16.667 58.6667L35.3337 40L16.667 21.3334L21.3337 16.6667L40.0003 35.3334L58.667 16.6667L63.3337 21.3334L44.667 40L63.3337 58.6667L58.667 63.3334L40.0003 44.6667L21.3337 63.3334Z" />
+        </svg>
+    </div> -->
+    <div id="nav-bar" class="nav-bar">
+
+        <a class="nav-item-1" href="views/main.view.php">Portfolio</a>
+
+        <a class="nav-item-2" href="views/about.view.php">About</a>
+        <a class="nav-item-3" href="views/contact.view.php">Contact</a>
+        <div class="icons-header">
             <!-- svg code from figma -->
             <div onclick="gitHublink()" class="icon-header">
                 <svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -43,20 +65,8 @@
 
             </div>
         </div>
-    </div> 
-    <div class="main-header">
-        <div class="backdrop-header"></div>
-        <div class="logo-container" onclick="homelink()">
-            <span class="fName-logo">Jesper</span>
-            <span class="lName-logo">Meuzelaar</span>
-        </div>
-        <div class="menu">
-            <svg width="30" height="30" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M10 40H70M10 20H70M10 60H70" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
-            </svg>
-        </div>
     </div>
-    
+    </div>
 </body>
 
 </html>
