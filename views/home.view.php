af54e91e7b79db7a3cb9d5e1d47b565847223529
Repsej07 <Jespermeuzelaar@gmail.com>
2024-10-09 01:44:08 +0200

    i have finished the whole home page, and made room for db implementations

--- views/home.view.php
+++ views/home.view.php
@@ -1,24 +1,58 @@
 <?php
 include "./views/inserts/header.php";
-$person_image = "./assets/Jesper.image.png";
+$person_image = "views/assets/Project-assets/Jesper.image.png";
+$title = "Home";
+$Skill1 = "HTML5";
+$Skill2 = "CSS";
+$Skill3 = "JavaScript";
+$Skill4 = "PHP";
+$Skill5 = "Adobe";
+$Skill6 = "Retail";
+$Skill7 = "MySQL";
+$Block1 = "views/assets/blocks/Sony-block.png";
+$Block2 = "views/assets/blocks/Mediamarkt-block.png";
+$Block3 = "views/assets/blocks/Code-block.png";
+$Block4 = "views/assets/blocks/BMD-block.png";
+$Contact = "views/assets/blocks/Contact-block.svg";
 ?>
 <html>
+
 <head>
-    <title>Home</title>
+    <title><?php echo $title ?></title>
 </head>
+
 <body>
     <div class="home-container">
-    <div class="main-image">
-    <img class="main-image-img"src="/views/assets/Project-assets/Jesper.image.png"  alt="main-image" >
+        <h1 class="title">Creating and selling Content</h1>
+        <div class="main-image">
+            <img class="main-image-img" src="<?php echo $person_image ?>" alt="main-image">
+        </div>
+        <div class="skill-bar">
+            <span class="skill1"><?php echo $Skill1 ?></span>
+            <span class="skill"><?php echo $Skill2 ?></span>
+            <span class="skill"><?php echo $Skill3 ?></span>
+            <span class="skill"><?php echo $Skill4 ?></span>
+            <span class="skill"><?php echo $Skill5 ?></span>
+            <span class="skill"><?php echo $Skill6 ?></span>
+            <span class="skill"><?php echo $Skill7 ?></span>
+        </div>
+
+
 
     </div>
-    <div class="bar">
-        <span>Skill1</span>
-       
-        <span>Skill2 </span>
-    </div>
+    <div class="block-grid">
+        <div class="block-1" style="background-image: url(<?php echo $Block1 ?>);"></div>
+        <div class="block-2" style="background-image: url(<?php echo $Block2 ?>);"></div>
+        <div class="block-3" style="background-image: url(<?php echo $Block3 ?>)"></div>
+        <div class="block-4" style="background-image: url(<?php echo $Block4 ?>)"></div>
+        <div class="contact-block" style="background-image:url(<?php echo $Contact ?>)">
+            <p class="contact-block-item">Curious?</p>
+           
+            <a class="contact-block-item-2" href="/contact">Contact</a>
+        </div>
     </div>
 </body>
-
+<?php
+include "./views/inserts/footer.php";?>
 
 </html>
