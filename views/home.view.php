2e878e8651d621bf3e6b0dc605c7e177ca68690b
Repsej07 <Jespermeuzelaar@gmail.com>
2024-10-08 23:22:42 +0200

    added a skill bar(not finished)

--- views/home.view.php
+++ views/home.view.php
@@ -7,8 +7,16 @@ $person_image = "./assets/Jesper.image.png";
     <title>Home</title>
 </head>
 <body>
+    <div class="home-container">
     <div class="main-image">
-    <img  src="/views/assets/Project-assets/Jesper.image.png" alt="main-image">
+    <img class="main-image-img"src="/views/assets/Project-assets/Jesper.image.png"  alt="main-image" >
+
+    </div>
+    <div class="bar">
+        <span>Skill1</span>
+       
+        <span>Skill2 </span>
+    </div>
     </div>
 </body>
 
