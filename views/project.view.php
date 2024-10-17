26ef63acba2fe27277a9d346931ee92efd2ad978
Repsej07 <Jespermeuzelaar@gmail.com>
2024-10-17 14:59:40 +0200

    commented a preset for when the project view needs input

--- views/project.view.php
+++ views/project.view.php
@@ -14,8 +14,8 @@ include("views/inserts/Header.php");
 <?php
    foreach ($content_array[0] as $content) {
     echo "<form class='article-project'>";
-    echo "<input type='text' class='header-project' value =" . "$content[article_header]". ">";
-    echo "<div class='article-project'>" . htmlspecialchars($content['article_content']) . "</div>";
+    echo "<div type='text' class='header-project'>".$content['article_header']."</div>";
+    echo "<div class='article-project'>".$content['article_content']."</div>";
     echo "</form>";
 }
 ?>
@@ -23,3 +23,12 @@ include("views/inserts/Header.php");
 include("views/inserts/Footer.php");
 ?>
 </html>
+
+<?php
+//example of what to load when there is a admin, makes the project editable
+//foreach ($content_array[0] as $content) {
+   // echo "<form class='article-project'>";
+    //echo "<input type='text' class='header-project'". "value = '$content[article_header]'". ">";
+    //echo "<textarea class='article-project' style='width:100%;' rows='12'>".$content['article_content']."</textarea>";
+    //echo "</form>";
+//}
