2f8b171a1f9f49450df96cea5b7859fc24a34357
Repsej07 <Jespermeuzelaar@gmail.com>
2024-10-17 18:29:07 +0200

    added ob

--- views/project.view.php
+++ views/project.view.php
@@ -1,17 +1,18 @@
 <?php
+ob_start();
 include("views/inserts/Header.php");
-
 ?>
 
 
 <head>
     <title>Project</title>
 </head>
-<div class="project-main-container">
-    
+<div class="project-main-container">  
 <img class="project-sub-image" src="<?=$content_array[0][0]['sub_image']?>">
 <img class="project-main-image" src="<?=$content_array[0][0]['main_image']?>"> 
 <?php
+
+
    foreach ($content_array[0] as $content) {
     echo "<form class='article-project'>";
     echo "<div type='text' class='header-project'>".$content['article_header']."</div>";
@@ -21,9 +22,9 @@ include("views/inserts/Header.php");
 ?>
 <?php
 include("views/inserts/Footer.php");
+ob_end_flush();
 ?>
 </html>
-
 <?php
 //example of what to load when there is a admin, makes the project editable
 //foreach ($content_array[0] as $content) {
