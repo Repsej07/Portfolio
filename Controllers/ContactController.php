509e01d7b55391631e50f9d649e0af328a16f3a7
Jesper <49792683+Repsej07@users.noreply.github.com>
2024-11-01 02:57:06 +0100

    Started on a contact form and the handling in the controller

--- Controllers/ContactController.php
+++ Controllers/ContactController.php
@@ -1,8 +1,30 @@
 <?php
 class ContactController
 {
-    public static function redirect($path = 'Contact', $args = null)
+
+
+    public static function index()
     {
-        require './views/' . $path . '.view.php';
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            self::handleFormSubmission();
+        } else {
+            // Load the contact form view
+            require './views/contact.view.php';
+        }
+        
     }
+   public static function  handleFormSubmission()
+    {
+        $name = $_POST['name'];
+        $mail = $_POST['mail'];
+        $message = $_POST['message'];
+       
+    $data = [
+        'name' => $name,
+        'mail' => $mail,
+        'message' => $message
+    ];
+    var_dump($data);
+    }
+
 }
--- views/contact.view.php
+++ views/contact.view.php
@@ -2,7 +2,21 @@
 require_once 'views/inserts/Header.php';
 ?>
 <html>
-   
+<form class="Contact-form" action="/contact" method="post">
+    <label for="name">First and/or Last name:</label>
+    <input class="contact-input" type="text" name="name" id="name-area" required />
+
+    <label for="mail">Email:</label>
+    <input class="contact-input" type="email" name="mail" id="mail-area" required />
+
+    <label for="message">Message:</label>
+    <textarea class="contact-input" name="message" id="message-area" rows="10" cols="50" required></textarea>
+
+    <button class="contact-button" type="submit">Submit</button>
+</form>
+
+
+   </form>
 </html>
 <?php
 require_once 'views/inserts/Footer.php';
