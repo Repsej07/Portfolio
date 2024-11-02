05053053410144d6fc9ec54af479a69228e277dd
Jesper <49792683+Repsej07@users.noreply.github.com>
2024-11-03 00:27:16 +0100

    I added the Scroll effect to the skillsbar

--- views/home.view.php
+++ views/home.view.php
@@ -4,23 +4,26 @@ include "./views/inserts/header.php";
 <html>
 
 <head>
-    <title><?=$result[0]['page_name']?></title>
+    <title><?= $result[0]['page_name'] ?></title>
 </head>
 
 <body>
     <div class="home-container">
-        <h1 class="title"><?=$result[0]['title_line']?></h1>
+        <h1 class="title"><?= $result[0]['title_line'] ?></h1>
 
         <div class="main-image">
-            <img class="main-image-img" src="<?=$result[0]['home_image']?>" alt="main-image">
+            <img class="main-image-img" src="<?= $result[0]['home_image'] ?>" alt="main-image">
         </div>
         <div class="skill-bar">
-            <?php
-            $skills = explode('|', $result[0]['skills']);
-            foreach ($skills as $index => $skill) {
-                echo "<span class='skill'".($index + 1)."'>".$skill."</span>";
-            }
-            ?>
+            <div class="skill-bar-inner">
+
+                <?php
+                $skills = explode('|', $result[0]['skills']);
+                foreach ($skills as $skill) {
+                    echo "<span class='skill'>" . htmlspecialchars($skill) . "</span>";
+                }
+                ?>
+            </div>
         </div>
 
 
@@ -31,13 +34,22 @@ include "./views/inserts/header.php";
         $blocks = explode('|', $result[0]['blocks']);
         foreach ($blocks as $index => $block) {
             $index = $index + 1;
-            echo"<div id='block' class=block-".($index)." "."onclick='link($index)'"."style=background-image:url($block)></div>";  
-        }?>
+            echo "<div id='block' class=block-" . ($index) . " " . "onclick='link($index)'" . "style=background-image:url($block)></div>";
+        } ?>
         <div class="block-5" style="background-image:url('views/assets/Contact-block.svg')">
-            <p class="block-5-item">Curious?</p>
+            <p class="block-5-item">Interested?</p>
             <a class="block-5-item-2" href="/contact">Contact</a>
-        </div> 
+        </div>
     </div>
+    <script>
+        document.addEventListener('scroll', () => {
+            const skillBarInner = document.querySelector('.skill-bar-inner');
+            const scrollY = window.scrollY;
+            skillBarInner.style.transform = `translateX(${scrollY / 5}px)`;
+        });
+    </script>
+    <!-- with help of gpt i have achieved the scroll effect for the skillsbar -->
+
 </body>
 <?php
 include "./views/inserts/footer.php"; ?>
