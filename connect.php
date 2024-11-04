65d3cfff776703989393b86a61f473f331281ecd
Jesper <49792683+Repsej07@users.noreply.github.com>
2024-11-04 21:25:36 +0100

    removed some unneccessary commentary

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "images";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      } catch(PDOException $e) {
+  }
+
