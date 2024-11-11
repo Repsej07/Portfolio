b253c04aae60cf8b467df932bf1c46004936bee6
Jesper <49792683+Repsej07@users.noreply.github.com>
2024-11-11 14:57:55 +0100

    It is not working

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -9,7 +9,7 @@ final class AdminController
     }
 
     static public function project_1(){
-        self::db_projects("sony");
+        $content_array [] = self::db_projects("1");
         require_once("./views/project.admin.view.php");
     }
     static public function db_projects($method){
@@ -18,8 +18,8 @@ final class AdminController
             $stmt = $db->prepare("SELECT main_image, sub_image,  p.name AS project_name, a.header AS article_header, a.content AS article_content
             FROM projects p
             JOIN articles a ON p.id = a.project_id
-            WHERE p.name = :method;");
-            $stmt->bindParam(':method', $method);
+            WHERE p.id = :id");
+            $stmt->bindParam(':id', $method);
             $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
             return $result;
@@ -27,6 +27,20 @@ final class AdminController
         } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
         }
+    }
+    public static function Project_adjustments()
+    {
+        $id = 1;
+        $name = "test";
+        $db = new PDO("mysql:host=localhost;dbname=jesper_portfolio", 'root', 'Hi123');
+        $stmt = $db->prepare("UPDATE projects SET name = :name, main_image = :main_image, sub_image = :sub_image WHERE id = :id");
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':main_image', $_POST['main_image']);
+        $stmt->bindParam(':sub_image', $_POST['sub_image']);
+        $id = '1';
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        header("location: /admin");
     }
 }
 /*
--- views/project.admin.view.php
+++ views/project.admin.view.php
@@ -6,8 +6,18 @@ include_once("views/inserts/Header.php");
     <title>Project Adjustments</title>
 </head>
 <body>
-    <form action="/admin/Project_adjustments">
+    <form action="/admin/Project_adjustments" method="POST">
         <textarea name="name" id="project_adjustments_name"><?=$content_array[0][0]['project_name']?></textarea>
+        <textarea name="sub_image" id="project_adjustments_sub_image"><?=$content_array[0][0]['sub_image']?></textarea>
+        <textarea name="main_image" id="project_adjustments_main_image"><?=$content_array[0][0]['main_image']?></textarea>
+        <?php
+        foreach ($content_array[0] as $content) {
+
+            echo "<textarea name='article_header' type='text'>".$content['article_header']."</textarea>";
+            echo "<textarea name='article_content' type='text'>".$content['article_content']."</textarea>";
+        }
+        ?>
+         <button type="submit">Submit</button>
 
 
 
