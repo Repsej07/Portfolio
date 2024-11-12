95cc5b816fa4cdd5b14793acfe517d8d416179b9
Jesper <49792683+Repsej07@users.noreply.github.com>
2024-11-12 09:13:11 +0100

    started on some admin page form styling

--- views/project.admin.view.php
+++ views/project.admin.view.php
@@ -7,14 +7,14 @@ include_once("views/inserts/Header.php");
 </head>
 <body>
     <form action="/admin/Project_adjustments" method="POST">
-        <textarea name="name" id="project_adjustments_name"><?=$content_array[0][0]['project_name']?></textarea>
-        <textarea name="sub_image" id="project_adjustments_sub_image"><?=$content_array[0][0]['sub_image']?></textarea>
-        <textarea name="main_image" id="project_adjustments_main_image"><?=$content_array[0][0]['main_image']?></textarea>
+        <textarea rows="1" cols="50" class="admin-textarea" name="name" id="project_adjustments_name"><?=$content_array[0][0]['project_name']?></textarea>
+        <textarea rows="2" cols="50" class="admin-textarea" name="sub_image" id="project_adjustments_sub_image"><?=$content_array[0][0]['sub_image']?></textarea>
+        <textarea rows="2" cols="50" class="admin-textarea" name="main_image" id="project_adjustments_main_image"><?=$content_array[0][0]['main_image']?></textarea>
         <?php
         foreach ($content_array[0] as $content) {
 
-            echo "<textarea name='article_header' type='text'>".$content['article_header']."</textarea>";
-            echo "<textarea name='article_content' type='text'>".$content['article_content']."</textarea>";
+            echo "<textarea rows='1' cols='50' class='admin-textarea' name='article_header' type='text'>".$content['article_header']."</textarea>";
+            echo "<textarea rows='8' cols='50' class='admin-textarea' name='article_content' type='text'>".$content['article_content']."</textarea>";
         }
         ?>
          <button type="submit">Submit</button>
@@ -23,7 +23,7 @@ include_once("views/inserts/Header.php");
 
 
 
-
+ 
 
 
 
